572a3f3c67d6393730fe55a62fabe6f909984d87
Alexander Schmidtberger <99815034+Kingsmann007@users.noreply.github.com>
2023-06-05 11:57:31 +0000

    ...

---
+++ checker.py
@@ -0,0 +1,6 @@
+print("choose an option: \n (0) = Add path \n (1) = check website")
+inp = input()
+if (inp == 0):
+    url = input("input path e.x. /index.html :")
+    f = open("urls.txt", "a")
+    f.write()
