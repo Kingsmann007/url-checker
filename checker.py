16a15904d65faddb4c83a010812f5ebc82316ce0
Alexander Schmidtberger <99815034+Kingsmann007@users.noreply.github.com>
2023-06-05 13:18:30 +0000

    nearly done

--- checker.py
+++ checker.py
@@ -1,6 +1,18 @@
-print("choose an option: \n (0) = Add path \n (1) = check website")
-inp = input()
-if (inp == 0):
-    url = input("input path e.x. /index.html :")
-    f = open("urls.txt", "a")
-    f.write()
+import requests
+def start():
+    print("choose an option: \n (0) = Add path \n (1) = check website")
+    inp = input()
+    if (inp == "0"):
+        url = input("input path e.x. /index.html :")
+        with open('urls.txt','a') as f:
+            f.write(f"{url}\n")
+    elif (inp == "1"):
+        url = input("input target URL: ")
+        with open('urls.txt','r') as f:
+            for line in f:
+                x = requests.get(url+line, timeout=5)
+                print(f'{url+line} = {x.status_code}')
+    else:
+        start()
+
+start()
